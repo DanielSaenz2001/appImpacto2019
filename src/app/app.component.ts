6e3bb8c727fb1330a9e62c92c0f1c7325fcb6e93
angel1ro <dotau2001@gmail.com>
2019-05-15 10:12:27 -0500

    tercero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { DownloadService } from '../app/download.service'
+import { element } from '@angular/core/src/render3';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,7 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   cont=0;
-  onDownload(){
-    this.cont=this.cont+1;
+  contList: Object[];
+
+  constructor(private downloadService:DownloadService){}
+  ngOnInit(){
+    this.downloadService.getContador().snapshotChanges().subscribe(item=>{
+      this.contList=[];
+      item.forEach(element=>{
+        let x=element.payload.toJSON();
+        x["$Key"]=element.key;
+          this.contList.push(x as object);
+      })
+    })
   }
 }
